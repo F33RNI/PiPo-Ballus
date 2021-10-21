9a3812f37314b8a051bcd53d43c9b62c6effb0dc
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2021-10-21 21:47:36 +0300

    Initial commit (android app)

---
+++ app/src/main/java/com/fern/pipo_ballus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fern.pipo_ballus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
