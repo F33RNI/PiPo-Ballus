48849617abdb333f799b94e295163b0158d5da97
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2021-10-31 04:56:24 +0300

    Add arduino project

---
+++ Arduino/PiPo-Ballus/Release/PiPo-Ballus.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\Frey\AppData\Local\Temp\VMBuilds\PiPo-Ballus\mega_atmega2560\Release\PiPo-Ballus.cpp.o: \
+ C:\Users\Frey\AppData\Local\Temp\VMBuilds\PiPo-Ballus\mega_atmega2560\Release\PiPo-Ballus.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h
