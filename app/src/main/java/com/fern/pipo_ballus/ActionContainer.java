34d3efdc0f2d20809e2cdba9f039c31200d1600b
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2021-11-02 01:00:32 +0300

    Add copyright and description to the ActionContainer.java

--- app/src/main/java/com/fern/pipo_ballus/ActionContainer.java
+++ app/src/main/java/com/fern/pipo_ballus/ActionContainer.java
@@ -1,5 +1,36 @@
+/*
+ * Copyright (C) 2021 Fern H. (aka Pavel Neshumov), PiPo-Ballus Android application
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * IT IS STRICTLY PROHIBITED TO USE THE PROJECT (OR PARTS OF THE PROJECT / CODE)
+ * FOR MILITARY PURPOSES. ALSO, IT IS STRICTLY PROHIBITED TO USE THE PROJECT (OR PARTS OF THE PROJECT / CODE)
+ * FOR ANY PURPOSE THAT MAY LEAD TO INJURY, HUMAN, ANIMAL OR ENVIRONMENTAL DAMAGE.
+ * ALSO, IT IS PROHIBITED TO USE THE PROJECT (OR PARTS OF THE PROJECT / CODE) FOR ANY PURPOSE THAT
+ * VIOLATES INTERNATIONAL HUMAN RIGHTS OR HUMAN FREEDOM.
+ * BY USING THE PROJECT (OR PART OF THE PROJECT / CODE) YOU AGREE TO ALL OF THE ABOVE RULES.
+ */
+
 package com.fern.pipo_ballus;
 
+/**
+ * This class is a container for action handler (in OpenCVHandler class)
+ */
 public class ActionContainer {
     public final static int ACTION_NONE = 0;
     public final static int ACTION_ROTATE_CW = 1;
@@ -13,5 +44,4 @@ public class ActionContainer {
     ActionContainer() {
         this.actionFrame = 0;
     }
-
 }
